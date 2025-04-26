5bd66526434edcae3ab4398f14dd9dd192645f2b
Micael Farias <micaelfarias.dev@gmail.com>
2025-04-25 22:50:55 -0300

    url das foto com caminho de acordo com usuario e data

--- user/models.py
+++ user/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
+
+
+def new_photo_path(instance, filename):
+    return f'photos/{instance.author.username}/{datetime.now().year}/{datetime.now().month}/{datetime.now().day}/{instance.title}'
 
 
 class Perfil(models.Model):
@@ -12,10 +17,11 @@ class Perfil(models.Model):
     def __str__(self):
         return self.user.username
 
+
 class Photos(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='photos/')
-    title = models.CharField(blank=True, null=True)
+    image = models.ImageField(upload_to=new_photo_path)
+    title = models.CharField(max_length=100, blank=False, null=False)
     views = models.IntegerField(default=0)
     description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
--- user/signals.py
+++ user/signals.py
@@ -1,9 +1,9 @@
 import os
-from django.db.models.signals import post_delete, post_save
+from django.db.models.signals import post_delete, post_save, pre_save
 from django.dispatch import receiver
 from .models import Photos, Perfil
 from django.contrib.auth.models import User
-
+from datetime import datetime
 
 
 @receiver(post_delete, sender=Photos)
@@ -11,6 +11,7 @@ def delete_photo_file(sender, instance, **kwargs):
     if instance.image:
         if os.path.isfile(instance.image.path):
             os.remove(instance.image.path)
+
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
