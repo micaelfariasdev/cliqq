59e9a5f384aec83cb48d0e194bf361985ec4b0a1
Micael Farias <micaelfarias.dev@gmail.com>
2025-04-25 21:52:54 -0300

    signal para criação do perfil assim que um novo user é criado

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Photos
+from .models import Photos, Perfil
 
 
 @admin.register(Photos)
@@ -7,3 +7,11 @@ class PhotoAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'author', 'created_at', 'views']
     search_fields = ['title', 'author__username']
     list_filter = ['created_at', 'author']
+
+
+@admin.register(Perfil)
+class PerfilAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'biografia',
+                    'telefone', 'endereco', 'data_nascimento']
+    search_fields = ['user']
+    list_filter = ['id', 'user']
--- user/signals.py
+++ user/signals.py
@@ -1,10 +1,18 @@
 import os
-from django.db.models.signals import post_delete
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
-from .models import Photos
+from .models import Photos, Perfil
+from django.contrib.auth.models import User
+
+
 
 @receiver(post_delete, sender=Photos)
 def delete_photo_file(sender, instance, **kwargs):
     if instance.image:
         if os.path.isfile(instance.image.path):
             os.remove(instance.image.path)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Perfil.objects.create(user=instance)
