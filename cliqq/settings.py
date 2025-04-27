55874468eab33f2210861a8ffab9b6a9b9ca61a3
Micael Farias <micaelfarias.dev@gmail.com>
2025-04-27 11:57:01 -0300

    allowed hosts

--- cliqq/settings.py
+++ cliqq/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&2egq@g*e#p^m#7qb!yw*hb^u8*_ypxjda=)rr=!is($6k$%^0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
